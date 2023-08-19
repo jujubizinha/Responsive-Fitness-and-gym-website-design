5c087ebaf70d932ca37d0c8382db84d223309491
Júlia Silvestre <juliasilvestreribeiro2004@gmail.com>
2023-08-19 19:35:18 -0300

    feat: create review section

--- script.js
+++ script.js
@@ -21,3 +21,21 @@ var swiper = new Swiper(".home-slider", {
     clickable: true,
   },
 });
+
+var swiper = new Swiper(".review-slider", {
+  spaceBetween: 20,
+  grabCursor: true,
+  loop: true,
+  autoplay: {
+    delay: 7500,
+    disableOnInteraction: false,
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    600: {
+      slidesPerView: 2,
+    },
+  },
+});
